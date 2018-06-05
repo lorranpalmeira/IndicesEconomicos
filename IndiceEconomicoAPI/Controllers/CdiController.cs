c6e2e230b70d5ee2319d22d68b1a1d1522813d5c
LORRAN PALMEIRA <lorranpalmeira@outlook.com>
2018-06-05 16:36:29 -0300

    # MediaCdi

--- IndiceEconomicoAPI/Controllers/CdiController.cs
+++ IndiceEconomicoAPI/Controllers/CdiController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System;
+using System.Globalization;
+using System.Net;
 
 namespace IndiceEconomicoAPI.Controllers
 {
@@ -27,5 +29,34 @@ namespace IndiceEconomicoAPI.Controllers
 
             return json;
         }
+
+        [HttpGet("MediaCdi")]
+        public string MediaCdi()
+        {
+            WebClient client = new WebClient();
+            DateTime data = DateTime.Today.AddDays(-1);
+            
+            string dataFormatada = data.ToString("yyyyMMdd");
+            var _mediaCdi = client.DownloadString(@"ftp://ftp.cetip.com.br/MediaCDI/"+ dataFormatada + ".txt");
+
+            _mediaCdi = _mediaCdi.Substring(5, 4);
+            _mediaCdi = _mediaCdi.Substring(0, 2) + "," + _mediaCdi.Substring(2, 2);
+            _mediaCdi = _mediaCdi.StartsWith("0") ? _mediaCdi.Substring(1, 4) : _mediaCdi;
+
+            var json = JsonConvert.SerializeObject(_mediaCdi);
+
+            return _mediaCdi;
+        }
+
+
+        [HttpGet("FatorDiario")]
+        public string FatorDiario()
+        {
+            //string json = JsonConvert.SerializeObject(indice);
+
+            return "json";
+        }
+
+
     }
 }
