0d466e7909683e1ae35f796fbb7ef58990e50af8
LORRAN PALMEIRA <lorranpalmeira@outlook.com>
2018-06-06 16:15:04 -0300

    # Moedas e Ipca

--- IndiceEconomicoAPI/Controllers/IpcaController.cs
+++ IndiceEconomicoAPI/Controllers/IpcaController.cs
@@ -1,16 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace IndiceEconomicoAPI.Controllers
 {
+
+    [Route("api/[controller]")]
     public class IpcaController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpGet("IpcaNumeroIndiceUltimoMes")]
+        public string IpcaNumeroIndiceUltimoMes() {
+
+            //WebClient client = new WebClient();
+            // var xml = client.DownloadString(@"http://api.sidra.ibge.gov.br/values/t/1737/n1/all//v/2266/p/201804");
+            string xml = new WebClient().DownloadString(@"http://api.sidra.ibge.gov.br/values/t/1737/n1/all//v/2266/p/201804");
+            
+            XDocument doc = XDocument.Parse(xml);
+            XmlNode myXmlNode = JsonConvert.DeserializeXmlNode(xml); // is node not note
+                                                                              // or .DeserilizeXmlNode(myJsonString, "root"); // if myJsonString does not have a root
+            string jsonString = JsonConvert.SerializeXmlNode(myXmlNode);
+
+            return jsonString;
+        }
+
     }
 }
--- IndiceEconomicoAPI/Controllers/MoedasController.cs
+++ IndiceEconomicoAPI/Controllers/MoedasController.cs
@@ -1,16 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace IndiceEconomicoAPI.Controllers
 {
+    [Route("api/[controller]")]
     public class MoedasController : Controller
     {
         public IActionResult Index()
         {
             return View();
+        }
+
+        //http://free.currencyconverterapi.com/api/v5/convert?q=USD_BRL&compact=y
+
+        [HttpGet("UsdBrl")]
+        public string UsdBrl() {
+
+            WebClient client = new WebClient();
+            var ret = client.DownloadString("http://free.currencyconverterapi.com/api/v5/convert?q=USD_BRL&compact=y");
+
+            //ret["val"].ToString()
+            var json = JsonConvert.SerializeObject(ret);
+            
+            
+
+            return ret;
         }
     }
 }
