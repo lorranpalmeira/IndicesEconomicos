c94f5b1263dfc8bae5810e7c744fe7aca082c170
LORRAN PALMEIRA <lorranpalmeira@outlook.com>
2018-06-08 11:31:56 -0300

    # Testes Moedas

--- IndiceEconomicoAPI/Controllers/MoedasController.cs
+++ IndiceEconomicoAPI/Controllers/MoedasController.cs
@@ -24,12 +24,24 @@ namespace IndiceEconomicoAPI.Controllers
 
             WebClient client = new WebClient();
             var ret = client.DownloadString("http://free.currencyconverterapi.com/api/v5/convert?q=USD_BRL&compact=y");
-
-
+            
             JObject rss = JObject.Parse(ret);
 
             string rssJson = (string)rss["USD_BRL"]["val"];
             return rssJson;
+        }
+
+        [HttpGet("UsdBrlShort")]
+        public string UsdBrlShort()
+        {
+            WebClient client = new WebClient();
+            var ret = client.DownloadString("http://free.currencyconverterapi.com/api/v5/convert?q=USD_BRL&compact=y");
+
+            JObject rss = JObject.Parse(ret);
+
+            string rssJson = (string)rss["USD_BRL"]["val"];
+            var realValue = rssJson.Substring(0,4);
+            return realValue;
         }
     }
 }
--- IndicesAPITestes/IndicesTestes/MoedasTestes.cs
+++ IndicesAPITestes/IndicesTestes/MoedasTestes.cs
@@ -9,15 +9,41 @@ namespace IndicesAPITestes.IndicesTestes
     public class MoedasTestes
     {
 
+        private MoedasController _moedas;
+
+        public MoedasTestes()
+        {
+            _moedas = new MoedasController();
+        }
+
         [Fact]
         public void UsdBrlAgoraIsGreaterThanZero() {
-            var usdbrl = new MoedasController();
-
-            var ret = usdbrl.UsdBrl();
+            
+            var ret = _moedas.UsdBrl();
 
             var validar = Convert.ToDouble(ret) > 0 ? true : false;
 
             Assert.True(validar);
         }
+
+        [Fact]
+        public void UsdBrlSizeIsValid()
+        {
+            var ret = _moedas.UsdBrl();
+
+            Assert.InRange(ret.Length, 8,8);
+
+            
+        }
+
+        [Fact]
+        public void UsdBrlShortSizeIsValid()
+        {
+            var ret = _moedas.UsdBrlShort();
+
+            Assert.InRange(ret.Length, 3, 5);
+            
+        }
+
     }
 }
