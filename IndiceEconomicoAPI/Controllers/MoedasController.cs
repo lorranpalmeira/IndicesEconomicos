147c4970da352155e660766aa4512092991d904a
LORRAN PALMEIRA <lorranpalmeira@outlook.com>
2018-06-10 11:03:08 -0300

    # Queries Moedas

--- IndiceEconomicoAPI/Controllers/MoedasController.cs
+++ IndiceEconomicoAPI/Controllers/MoedasController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using IndiceEconomicoAPI.Queries;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -13,19 +14,30 @@ namespace IndiceEconomicoAPI.Controllers
     public class MoedasController : Controller
     {
 
+        private MoedasQueries _moedas;
+
+        public MoedasController()
+        {
+            _moedas = new MoedasQueries();
+        }
+
 
         //http://free.currencyconverterapi.com/api/v5/convert?q=USD_BRL&compact=y
 
         [HttpGet("UsdBrl")]
         public string UsdBrl() {
 
+            
             WebClient client = new WebClient();
             var ret = client.DownloadString("http://free.currencyconverterapi.com/api/v5/convert?q=USD_BRL&compact=y");
+            
             
             JObject rss = JObject.Parse(ret);
 
             string rssJson = (string)rss["USD_BRL"]["val"];
+
             return rssJson;
+
         }
 
         [HttpGet("UsdBrlShort")]
---
+++ IndiceEconomicoAPI/Queries/MoedasQueries.cs
@@ -0,0 +1,36 @@
+﻿using IndiceEconomicoAPI.Indices;
+using IndiceEconomicoAPI.MongoDriver;
+using MongoDB.Driver;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace IndiceEconomicoAPI.Queries
+{
+    public class MoedasQueries
+    {
+
+        private MongoDbContext _dbContext;
+
+        public MoedasQueries()
+        {
+            _dbContext = new MongoDbContext();
+        }
+
+        public List<Moedas> UsdBrl() {
+
+           var result = _dbContext.Moedas.Find(x => true).ToList();
+
+            //var valor =
+             //   from x in _dbContext.Moedas.Find(m => true);
+               // where x. == DateTime.Now.ToLocalTime().ToString("dd/MM/yyyy")
+               // select (x.);
+
+            return result;
+        }
+
+    }
+}
